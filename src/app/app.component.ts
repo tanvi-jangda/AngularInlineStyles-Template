cc5971497259a4062529ce88c55164455a0ed062
tanvi-jangda <tanvi.jangda@gmail.com>
2023-04-25 16:11:06 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h3>Inline template</h3>
+  <p>Second line of inline template</p>
+  `,
+  styles: [`
+  h3{color:blue; font-weight:normal} p{color:green; font-weight:bold }`
+]
 })
 export class AppComponent {
   title = 'April16';
---
+++ src/app/componentinline/componentinline.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componentinline',
+  template: `
+    <p>
+      componentinline works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentinlineComponent {
+
+}
